47dfd297fabdebb2d833df4422dd9a40879aa1c5
hiiiia <90683622+hiiiia@users.noreply.github.com>
2024-07-25 10:48:04 +0900

    feat: 시간표에 강의 생성, 삭제 추가 / DB 설정
    @app.post('/api/v1/course-timetables')
    code로 CourseId를 전부 찾고
    CourseTimetable에 강의 등록
    
    @app.delete('/api/v1/course-timetables')
    code로 CourseId를 전부 찾고
    CourseTimetable에 해당 강의 삭제

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Body, HTTPException
+from model import *
+from db import session
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy import and_
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True, 
+    allow_methods=["*"],     
+    allow_headers=["*"],     
+)
+
 
 
 @app.get("/")
@@ -9,5 +24,103 @@ async def root():
 
 
 @app.get("/hello/{name}")
-async def say_hello_name(name: str):
+async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+
+@app.post('/api/v1/course-timetables')
+async def postCourseToTimetable(userId : int = Body(...,embed=True),
+               timetableId : int = Body(...,embed=True),
+               code : str = Body(...,embed=True)):
+    
+
+    # code로 courseId 찾기
+    course = session.query(Course.course_id).filter(Course.code == code).all()
+    if not course:
+        return {
+            "success" : False,
+            "data" : "Invalid code",
+            "error" : None    
+            }
+
+
+    timetable = session.query(Timetable).filter(Timetable.timetable_id == timetableId).first()
+    if not timetable:
+        return {
+            "success" : False,
+            "data" : "Invalid timetable_id",
+            "error" : None    
+            }
+    
+
+    for courseId in course:
+        courses = CourseTimetable()
+        courses.timetable_id = timetableId
+        courses.course_id = courseId[0]
+
+        try :
+            session.add(courses)
+            session.commit()
+            session.refresh(courses)
+
+        except SQLAlchemyError as e:
+            return {
+                "success" : False,
+                "data" : None,
+                "error" : e
+            }
+        
+    return{
+        "success" : True,
+        "data" : None,
+        "error" : None  
+    }
+
+@app.delete('/api/v1/course-timetables')
+async def deleteCourseFromTimetable(userId : int = Body(...,embed=True),
+               timetableId : int = Body(...,embed=True),
+               code : str = Body(...,embed=True)):
+    
+
+    # code로 courseId 찾기
+    course = session.query(Course.course_id).filter(Course.code == code).all()
+    if not course:
+        return {
+            "success" : False,
+            "data" : "Invalid code",
+            "error" : None    
+            }
+
+
+    timetable = session.query(Timetable).filter(Timetable.timetable_id == timetableId).first()
+    if not timetable:
+        return {
+            "success" : False,
+            "data" : "Invalid timetable_id",
+            "error" : None    
+            }
+    
+
+    for courseId in course:
+
+        try :
+            courseId[0]
+            session.query(CourseTimetable).filter(and_(
+                CourseTimetable.course_id == courseId[0],
+                CourseTimetable.timetable_id == timetableId)).delete()
+            session.commit()
+
+        except SQLAlchemyError as e:
+            return {
+                "success" : False,
+                "data" : None,
+                "error" : e
+            }
+        
+    return{
+        "success" : True,
+        "data" : None,
+        "error" : None  
+    }
+
